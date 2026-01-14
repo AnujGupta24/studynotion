d09de9ea96af2f38cb36869a2260c38f9291a481
AnujGupta24 <anujgupta4594@gmail.com>
2026-01-14 16:30:38 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import OpenRoute from './components/core/Auth/OpenRoute';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ForgotPassword from './pages/ForgotPassword';
-import UpdatePassword from './pages/updatePassword';
+import UpdatePassword from './pages/UpdatePassword';
 import VerifyEmail from './pages/VerifyEmail';
 import About from './pages/About';
 import Contact from './pages/Contact';
