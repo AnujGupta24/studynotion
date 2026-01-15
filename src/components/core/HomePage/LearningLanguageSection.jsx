876ce1e7c53522574913a15c0d74865a62881697
AnujGupta24 <anujgupta4594@gmail.com>
2026-01-15 14:23:16 +0530

    fix: correct capitalization in import paths for images in LearningLanguageSection component

--- src/components/core/HomePage/LearningLanguageSection.jsx
+++ src/components/core/HomePage/LearningLanguageSection.jsx
@@ -1,7 +1,7 @@
 import HighlightText from './HighlightText';
-import know_your_progress from '../../../assets/Images/know_your_progress.png';
-import compare_with_others from '../../../assets/Images/compare_with_others.png';
-import plan_your_lessons from '../../../assets/Images/plan_your_lessons.png';
+import know_your_progress from '../../../assets/Images/Know_your_progress.png';
+import compare_with_others from '../../../assets/Images/Compare_with_others.png';
+import plan_your_lessons from '../../../assets/Images/Plan_your_lessons.png';
 import CTAButton from '../HomePage/Button';
 function LearningLanguageSection() {
 	return (
@@ -9,28 +9,16 @@ function LearningLanguageSection() {
 			<div className="flex flex-col items-center gap-5">
 				<h2 className="text-4xl text-center font-semibold">
 					Your Swiss Knife for{' '}
-					<HighlightText
-						text={'learn any language'}
-						className="font-bold text-richblue-200"
-					/>
+					<HighlightText text={'learn any language'} className="font-bold text-richblue-200" />
 				</h2>
 				<p className="text-richblack-600 mx-auto text-center font-medium w-[70%]">
-					Using spin making learning multiple languages easy. with 20+
-					languages realistic voice-over, progress tracking, custom
-					schedule and more.
+					Using spin making learning multiple languages easy. with 20+ languages realistic voice-over,
+					progress tracking, custom schedule and more.
 				</p>
 
 				<div className="flex items-center justify-center mt-3">
-					<img
-						src={know_your_progress}
-						className="object-contain -mr-44"
-						alt="knowyourprogressimage "
-					/>
-					<img
-						src={compare_with_others}
-						className="object-contain"
-						alt="comparewithothersimage"
-					/>
+					<img src={know_your_progress} className="object-contain -mr-44" alt="knowyourprogressimage " />
+					<img src={compare_with_others} className="object-contain" alt="comparewithothersimage" />
 					<img
 						src={plan_your_lessons}
 						className="object-contain -ml-36 mb-auto "
